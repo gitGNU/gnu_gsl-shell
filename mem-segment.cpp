86018bbe079487886d9167db1ccb31b0b09b5bab
francesco <francesco.bbt@gmail.com>
2011-01-02 01:56:22 +0100

    added linux/unix specific implementation for executable memory allocation

--- mem-segment.cpp
+++ mem-segment.cpp
@@ -1,11 +1,12 @@
 
+#include "mem-segment.h"
+
+#ifdef WIN32
 #include <windows.h>
 
 /*
 void * __attribute__((__stdcall__)) VirtualAlloc(void *, unsigned long, unsigned long, unsigned long);
 */
-
-#include "mem-segment.h"
 
 unsigned char *
 mem_segment_alloc (int sz)
@@ -13,3 +14,18 @@ mem_segment_alloc (int sz)
   void * p = ::VirtualAlloc(0, sz, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
   return (unsigned char *) p;
 }
+#else
+
+#define __USE_MISC
+#include <sys/mman.h>
+
+unsigned char *
+mem_segment_alloc (int sz)
+{
+  void * p = mmap(0, sz, 
+		  PROT_EXEC | PROT_READ | PROT_WRITE,
+		  MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  return (unsigned char *) p;
+}
+
+#endif
